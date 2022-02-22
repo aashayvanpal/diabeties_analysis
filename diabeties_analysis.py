9862426117d5355a0d43c805f9d342a033552835
Aashay <arkhamknight.aashay@gmail.com>
2022-02-23 02:09:08 +0530

    Initial commit

---
+++ diabeties_analysis.py
@@ -0,0 +1,12 @@
+# importing packages 
+
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+# Reading the dataframe
+
+df = pd.read_csv('diabetes.csv')
+
+print(df.head())
